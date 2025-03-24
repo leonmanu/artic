9b9614dcda734fca2c41537f6fa17df37c550a2d
Manuel <leonmanu@gmail.com>
2025-03-24 11:27:25 -0300

    cambié la fila de  credentials

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -3,8 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const fs = require('fs');
 
 // Carga las credenciales desde client_secret.json
-const credentials = process.env.GOOGLE_CLIENT_SECRET ? JSON.parse(process.env.GOOGLE_CLIENT_SECRET): {};
-console.log("CONFIG_PASSPORT:", process.env.CONFIG_PASSPORT);
+const credentials = JSON.parse(process.env.GOOGLE_CLIENT_SECRET);
 
 // Extrae los valores necesarios
 const CLIENT_ID = credentials.web.client_id;
