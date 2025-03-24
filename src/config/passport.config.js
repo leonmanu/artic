c21b4537ab2a64b852129caef4b9870271770471
Manuel <leonmanu@gmail.com>
2025-03-24 09:48:44 -0300

    first commit

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -3,7 +3,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const fs = require('fs');
 
 // Carga las credenciales desde client_secret.json
-const credentials = JSON.parse(process.env.GOOGLE_CLIENT_SECRET);
+const credentials = process.env.GOOGLE_CLIENT_SECRET ? JSON.parse(process.env.GOOGLE_CLIENT_SECRET);
+console.log("CONFIG_PASSPORT:", process.env.CONFIG_PASSPORT);
 
 // Extrae los valores necesarios
 const CLIENT_ID = credentials.web.client_id;
